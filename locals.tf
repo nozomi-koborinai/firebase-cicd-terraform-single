13f0e3319100fa6dd13baab42852460931a926d9
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2023-08-30 20:02:58 +0900

    feat: create firebase infrastructure

---
+++ locals.tf
@@ -0,0 +1,11 @@
+# 各種リソース共通の定数群
+locals {
+  # リージョンコード：東京
+  region = "asia-northeast1"
+
+  # プロジェクトID
+  project_id = "firebase-cicd-terraform-single"
+
+  # プロジェクト名
+  project_name = "firebase-cicd-terraform-single"
+}
